2cbee3fe0f987777321c96d48610334b9c046130
Olotse <antonin.istia@gmail.com>
2025-02-10 04:09:13 +0100

    Close #9

---
+++ Editor/Utils/DefineSymbolsUtils.cs
@@ -0,0 +1,56 @@
+/// Copyright 2025, Antonin Boureau, All rights reserved.
+/// Version 20250210
+
+using UnityEditor;
+
+namespace Devloader.Utils.EditorOnly
+{
+    public class DefineSymbolsUtils
+    {
+        const char SymbolSeparator = ';';
+
+        List<string> _symbols = new List<string>();
+        public string[] symbols => _symbols.ToArray();
+
+        BuildTargetGroup _group;
+
+        /// <summary>
+        /// Create a Symbol Parser using selected build target group to select scripting defines
+        /// </summary>
+        public SymbolParser() : this(PlayerSettings.GetScriptingDefineSymbolsForGroup(EditorUserBuildSettings.selectedBuildTargetGroup), EditorUserBuildSettings.selectedBuildTargetGroup) { }
+
+        /// <summary>
+        /// Create a Symbol Parser using given build target group to select scripting defines
+        /// </summary>
+        public SymbolParser(BuildTargetGroup group) : this(PlayerSettings.GetScriptingDefineSymbolsForGroup(group), group) { }
+
+        /// <summary>
+        /// Create a Symbol Parser using given symbols string
+        /// </summary>
+        public SymbolParser(string symbols, BuildTargetGroup group)
+        {
+            _symbols = new List<string>(symbols.Split(SymbolSeparator));
+            _group = group;
+        }
+
+        public void Add(string symbol) => _symbols.Add(symbol);
+
+        public bool Check(string symbol) => _symbols.Contains(symbol);
+
+        public bool Remove(string symbol) => _symbols.Remove(symbol);
+
+        public void Replace(string oldSymbol, string newSymbol)
+        {
+            Remove(oldSymbol);
+            Validate(newSymbol);
+        }
+
+        public void Save() => PlayerSettings.SetScriptingDefineSymbolsForGroup(_group, string.Join(SymbolSeparator, symbols));
+
+        public void Validate(string symbol)
+        {
+            if (!Check(symbol))
+                Add(symbol);
+        }
+    }
+}
