79940084aba7ebee7f469439d0132d78ab1bee3e
Olotse <antonin.istia@gmail.com>
2025-08-11 16:36:13 +0200

    Amélioration de PhysicsFollowTransform

--- Runtime/Physics/PhysicsFollowTransform.cs
+++ Runtime/Physics/PhysicsFollowTransform.cs
@@ -1,53 +1,44 @@
-using Devloader.Extensions;
+/// Copyright 2025, Antonin Boureau, All rights reserved.
+/// Version 20250811
+
 using UnityEngine;
 
-/// <summary>
-/// Script permettant à un rigidbody de suivre le transform d'un autre objet
-/// </summary>
-public class PhysicsFollowTransform : MonoBehaviour
+namespace Devloader
 {
-    [SerializeField] Rigidbody rb;
-    [SerializeField] Transform targetTransform;
-    [Space]
-
-    [SerializeField] bool useFixedUpdate;
-
-#if UNITY_EDITOR
-    private void OnValidate()
+    /// <summary>
+    /// Script permettant à un rigidbody de suivre le transform d'un autre objet
+    /// </summary>
+    [RequireComponent(typeof(Rigidbody))]
+    public class PhysicsFollowTransform : MonoBehaviour
     {
-        if(!rb)
-            rb = this.ValidateComponent<Rigidbody>();
+        [SerializeField] Rigidbody rb;
+        [SerializeField] Transform targetTransform;
+        [Space]
+
+        [SerializeField] bool useFixedUpdate;
+
+        private void FixedUpdate()
+        {
+            if (!useFixedUpdate || !targetTransform)
+                return;
+
+            MoveRigidBodyToTransform(targetTransform);
+        }
+
+        private void Update()
+        {
+            if (useFixedUpdate || !targetTransform)
+                return;
+
+            MoveRigidBodyToTransform(targetTransform);
+        }
+
+        public void MoveRigidBodyToTransform(Transform transform)
+        {
+            if (transform)
+                MoveRigidBodyToPosition(transform.position);
+        }
+
+        public void MoveRigidBodyToPosition(Vector3 position) => rb.MovePosition(position);
     }
-
-#endif
-
-    private void Start()
-    {
-        if (!rb)
-            rb = this.ValidateComponent<Rigidbody>();
-    }
-
-    private void FixedUpdate()
-    {
-        if (!useFixedUpdate)
-            return;
-
-        MoveRigidBodyToTransform(targetTransform);
-    }
-
-    private void Update()
-    {
-        if (useFixedUpdate)
-            return;
-
-        MoveRigidBodyToTransform(targetTransform);
-    }
-
-    private void MoveRigidBodyToTransform(Transform transform)
-    {
-        if (transform)
-            MoveRigidBodyToPosition(transform.position);
-    }
-
-    private void MoveRigidBodyToPosition(Vector3 position) => rb.MovePosition(position);
 }
