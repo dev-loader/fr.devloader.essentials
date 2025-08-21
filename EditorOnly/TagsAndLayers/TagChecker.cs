9b37f53ce390c5f48f4e0cd37cfa1e7ffeb20c1d
Olotse <antonin.istia@gmail.com>
2025-08-21 21:58:22 +0200

    Close #37

---
+++ EditorOnly/TagsAndLayers/TagChecker.cs
@@ -0,0 +1,44 @@
+/// Copyright 2025, Antonin Boureau, All rights reserved.
+/// Version 20250821
+
+using UnityEngine;
+
+using Devloader.Utils.EditorOnly;
+using System.ComponentModel;
+using System.Collections.Generic;
+
+namespace Devloader.EditorOnly.TagsAndLayers
+{
+    [DisplayName("[Editor Only] Tag Checker")]
+    public class TagChecker : MonoBehaviour
+    {
+        [SerializeField] private string _tagName;
+
+        List<string> protectedTags = new List<string>(new string[7]
+        {
+            "Untagged",
+            "Respawn",
+            "Finish",
+            "EditorOnly",
+            "MainCamera",
+            "Player",
+            "GameController"
+        });
+
+        public void CheckTag()
+        {
+            if (!protectedTags.Contains(_tagName))
+                TagUtils.Validate(_tagName);
+
+            gameObject.tag = _tagName;
+        }
+
+        public void DeleteTag(string tagName)
+        {
+            if (!protectedTags.Contains(tagName))
+                TagUtils.Remove(tagName);
+        }
+
+        public string TagName => _tagName;
+    }
+}
